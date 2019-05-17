53088db8090757f5ffd8b4e73a7248900336054c
dojo-json <jfranz@codingdojo.com>
2019-05-17 15:23:44 -0700

    create a book

--- week2/day4/books/src/app/app.component.ts
+++ week2/day4/books/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { Book } from './models';
+import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'BooAngularks';
+  book = new Book();
+  books: Book[] = [];
+
+  onSubmit(event: Event, form: NgForm) {
+    event.preventDefault();
+    console.log('submitting form', this.book);
+
+    // push book from form/book instance to books
+    this.books.push(this.book);
+
+    // no longer refers to the book we just created
+    this.book = new Book();
+
+    form.reset();
+
+    console.log(this.books);
+  }
 }
