544f58ee270fc1c90fdab2268008ac174a3dc6af
dojo-json <jfranz@codingdojo.com>
2019-05-17 10:43:43 -0700

    express + angular

---
+++ week2/day4/books/server.js
@@ -0,0 +1,16 @@
+const parser = require('body-parser');
+const express = require('express');
+const path = require('path');
+
+const {
+  env: { PORT: port = 8000 },
+} = process;
+const app = express();
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+app.use(express.static(path.join(__dirname, 'dist/books')));
+
+console.log(path.join(__dirname, 'dist/books'));
+
+app.listen(port, () => console.log(`express server listening on port ${port}`));
